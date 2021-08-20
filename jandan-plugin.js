c9a76f61de80aa22bed4f3e7d46d3a1cc9a19d2d
zhuT <371870148@qq.com>
2021-08-20 11:01:51 +0800

    add more part about top

--- jandan-plugin.js
+++ jandan-plugin.js
@@ -5,25 +5,17 @@ const fs = require("fs");
 const YAML = require("yamljs")
 const { segment } = require('koishi');
 const config = loadYAMLFile("./config.yaml");
-let baseUrl = config.jandan.baseUrl;
-let top = config.jandan.top;
-let top4h = config.jandan.top4h;
-let topTucao = config.jandan.topTucao;
-let topOoxx = config.jandan.topOoxx;
-let topZoo = config.jandan.topZoo;
-let topComments = config.jandan.topComments;
-let top3days = config.jandan.top3days;
-let top7days = config.jandan.top7days;
+const baseUrl = config.jandan.baseUrl;
 let time = config.jandan.refreshInterval;
-
+let cmds = config.jandan.cmds;
 const storage = {
     updateTime:"",
-    dataList:[],
+    dataList:{},
     usersPoint:{},
     views:[]
 }
 // todo update url concat
-// add more part
+// 
 // add log
 // use database stroe date
 // format double number %.2f
@@ -31,53 +23,51 @@ const storage = {
 // pic size too large emit
 // pic cache 
 // add manual reset index
-async function getTop(session) {
+// fix timeout NaN
+async function jandan(session) {
     let cid = session.cid;
+    const type = session.content;
     this.session = session;
     let index = 0;
     if (storage.usersPoint[cid] === undefined) {
-        storage.usersPoint[cid] = 0;
-    } else {
-        index = storage.usersPoint[cid];
+        storage.usersPoint[cid] = new Object();   
+    } 
+    if (storage.usersPoint[cid][type] === undefined) {
+        storage.usersPoint[cid][type] = 0;
     }
-    storage.usersPoint[cid] ++;
+    index = storage.usersPoint[cid][type];
+    storage.usersPoint[cid][type] ++;
 
-    if(index > storage.dataList.length ) {
+    if(index > storage.dataList[type].length ) {
         session.send(`没了看完了等${time/60}分钟后重置吧${segment("face",{id:"174"})}`);
     } 
 
-    let result = storage.dataList[index];
-    let results = [];
+    let result = storage.dataList[type][index];
+    let response = [];
     let posText = segment("face",{id:"76"});
     let negText = segment("face",{id:"77"});
     let posVal = result.pos;
     let negVal = result.neg;
-    results.push(`${posText}:${posVal}\t${negText}:${negVal}`);
-    results.push(segment("text",{content:result.content}));
+    response.push(`${posText}:${posVal}\t${negText}:${negVal}`);
+    response.push(segment("text",{content:result.content}));
     for (let i = 0; i < result.imgs.length; i++) {
         const img = result.imgs[i];
-        results.push(segment("image",{
+        response.push(segment("image",{
             url:`http:${img}`,
             timeout:`5000`,
             cache:true,
         }));
     }
-    results.push(`/t/${result.id}`);
+    response.push(`/t/${result.id}`);
     
     
-    session.send(results.join("\n"));
-    // session.send(segment("share",{
-    //     url:baseUrl+`/t/${result.id}`,
-    //     title:"jandan",
-    //     content:`/t/${result.id}`
-    // }));
+    session.send(response.join("\n"));
 }
 
 function loadYAMLFile(file) {
     return YAML.parse(fs.readFileSync(file).toString());
 }
 
-analyzeAndSave(baseUrl + top4h,storage);
 function request(url) {
     return axios.get(url)
 }
@@ -94,49 +84,61 @@ var t = setInterval(function () {
     }
 }, 1000)
 
-async function analyzeAndSave(url,storage) {
-    console.log("get data...")
-    let html = await request(url);
-    const $ = cheerio.load(html.data);
-    let commentList = $("ol.commentlist li");
-    for (let i=0;i< commentList.length;i++) {
-        let el = commentList[i];
-        let url = $(".author a",el).attr("name");
-        let imgs = [];
-        let imgUrls = $("p a",el);
-        for (let i = 0; i < imgUrls.length; i++) {
-            const img = imgUrls[i];
-            imgs.push(img.attribs.href)
+async function analyzeAndSave(...types) {
+    for (let i = 0; i < types.length; i++) {
+        const type = types[i];
+        const url = baseUrl + cmds[type];
+        let html = await request(url);
+        const $ = cheerio.load(html.data);
+        let commentList = $("ol.commentlist li");
+        for (let i=0;i< commentList.length;i++) {
+            let el = commentList[i];
+            let name = $(".author a",el).attr("name");
+            let imgs = [];
+            let imgUrls = $("p a",el);
+            for (let i = 0; i < imgUrls.length; i++) {
+                const img = imgUrls[i];
+                imgs.push(img.attribs.href)
+            }
+            imgs = imgs.reverse();
+            let content = $("p",el).text();
+            let pos = $(".jandan-vote span",el)[0].children[0].data;
+            let neg = $(".jandan-vote span",el)[1].children[0].data;
+            let commentObj = {
+                id:name,
+                content:content,
+                imgs:imgs,
+                pos:pos,
+                neg:neg,
+            }
+            if (!storage.views.includes(name)) {
+                storage.dataList[type].push(commentObj);
+                storage.views.push(name);
+            }
         }
-        imgs = imgs.reverse();
-        let content = $("p",el).text();
-        let pos = $(".jandan-vote span",el)[0].children[0].data;
-        let neg = $(".jandan-vote span",el)[1].children[0].data;
-        let commentObj = {
-            id:url,
-            content:content,
-            imgs:imgs,
-            pos:pos,
-            neg:neg,
-        }
-        if (!storage.views.includes(url)) {
-            storage.dataList.push(commentObj);
-            storage.views.push(url);
-        }
+        storage.dataList[type] =  storage.dataList[type].reverse();
+        storage.updateTime = new Date();
     }
-    storage.dataList =  storage.dataList.reverse();
-    storage.updateTime = new Date();
 }
 
 
+async function init() {
+    console.log("init...");
+    const keys = Object.keys(cmds);
+    for (i of keys) {
+        storage.dataList[i] = [];
+    }
+    await analyzeAndSave(...keys);
+}
+init();
 
 module.exports = (ctx) => {
     
 
     ctx.middleware((session, next) => {
-      if (session.content === 'd') {
+      if (Object.keys(cmds).includes(session.content)) {
           session.send(`少男祈祷中...${segment("face",{id:"190"})}`);
-          getTop(session);
+          jandan(session);
       }
       return next() 
     })
